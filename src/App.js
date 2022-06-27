feb02a63b17b1d8e21668b74e356d407058d9c17
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-06-27 11:53:53 +0200

    naslov

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ export default class App extends React.Component {
     render() {
         return(
             <div>
-                
+                <h1>Praksa</h1>
+
             </div>
         )
     }
