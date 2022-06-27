649e10f1f2cbd6484741ac16cf8e598a5fd0421d
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-06-27 12:54:30 +0200

    tabela

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Drivers from "./components/Drivers";
 
 export default class App extends React.Component {
     render() {
         return(
             <div>
                 <h1>Praksa</h1>
+                <Drivers />
 
             </div>
         )
