23d74f1d25bcd605d505bbf087b41620e48f1b1c
Jelena Njegovan <elegoo@gmail.com>
2022-06-27 12:44:46 +0200

    Ovo je Stevan

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -4,7 +4,7 @@ export default class Drivers extends React.Component {
     render() {
         return(
             <div>
-                
+                <h1>Drivers</h1>
             </div>
         )
     }
--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,10 +1,24 @@
 import React from "react";
+import * as $ from "jquery";
 
 export default class Teams extends React.Component {
+
+    state= {
+        teams:[]
+    }
+
+    componentDidMount(){
+        const url = "http://ergast.com/api/f1/2013/constructorStandings.json";
+        $.get(url, (data)=>{
+            this.setState({
+                teams: data
+            })
+        })
+    }
     render() {
         return(
             <div>
-                
+                {}
             </div>
         )
     }
