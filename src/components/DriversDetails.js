84144defe715a4198d4a9354157f2943d9f82652
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-06-28 13:29:24 +0200

    driversDetails

--- src/components/DriversDetails.js
+++ src/components/DriversDetails.js
@@ -4,6 +4,8 @@ import { useLocation } from "react-router-dom";
 
 const DriversDetails = () => {
     const [details, getDetails] = useState([]);
+    const [races, getRaces] = useState([])
+   // const [pictures, getPictures] = useState([]);
     const location = useLocation();
 
     useEffect(() => {
@@ -19,8 +21,63 @@ const DriversDetails = () => {
         })
     }
 
-    return(
+    useEffect(() => {
+        addRaces()
+     }, [])
+     const addRaces = () => {
+         const url = "http://ergast.com/api/f1/2013/results/1.json";
+         $.get(url, (data) => {
+             getRaces(data.MRData.RaceTable.Races)
+         })
+     }
+
+    return (
         <div>
+            {/* <table>
+                <tbody>
+                    {details.map(driver => {
+                        return(
+                            <tr>
+                            <td>{details.givenName}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        )
+                    })}
+               
+                </tbody>    
+            </table> */}
+
+            
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan={5}>Formula 1 2013 Results</th>
+                    </tr>
+                    <tr>
+                        <th>Round</th>
+                        <th>Grand Prix</th>
+                        <th>Team</th>
+                        <th>Grid</th>
+                        <th>Race</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {races.map(item => {
+                        console.log(item);
+                        return (
+                            <tr key={item.round}>
+                                <td>{item.round}</td>
+                                <td>{item.raceName}</td>
+                                <td>{item.Results[0].Constructor.name}</td>
+                                <td>{item.Results[0].grid}</td>
+                                <td>{item.Results[0].position}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
 
         </div>
     )
