c9c9352fdcb2c4ae026360fa8395239028594b4e
Biki Miroslav <biki.miroslav@gmail.com>
2022-06-28 08:05:35 +0200

    dodata komponenta Races

--- src/components/Races.js
+++ src/components/Races.js
@@ -1,10 +1,55 @@
 import React from "react";
+import * as $ from "jquery";
 
 export default class Races extends React.Component {
+
+    state = {
+        races: []
+    }
+
+    componentDidMount() {
+        this.getRaces()
+    }
+
+    getRaces = () => {
+        const url = "http://ergast.com/api/f1/2013/results/1.json";
+        $.get(url, (data) => {
+            console.log("trke", data.MRData.RaceTable.Races);
+            this.setState({
+                races: data.MRData.RaceTable.Races
+            })
+        })
+    }
+
     render() {
-        return(
+        return (
             <div>
-                <h1>Races</h1>
+                <h3>Races Championship - 2013</h3>
+                <table>
+                    <thead>
+                        <th>Round</th>
+                        <th>Grand Prix</th>
+                        <th>Circuit</th>
+                        <th>Date</th>
+                        <th>Winner</th>
+                    </thead>
+                    <tbody>
+                        {this.state.races.map(item => {
+                            return (
+                                <tr>
+                                    <td>{item.round}</td>
+                                    <td>{item.raceName}</td>
+                                    <td>{item.Circuit.circuitName}</td>
+                                    <td>{item.date}</td>
+                                    <td>{item.Results[0].Driver.familyName}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+
+
+                <h1>Race Calendar</h1>
             </div>
         )
     }
