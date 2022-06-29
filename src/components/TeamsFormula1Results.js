209ce69b31c106daab0f8443f682526fa030598a
Jelena Njegovan <elegoo@gmail.com>
2022-06-29 15:06:10 +0200

    TeamsFormula1Results update

--- src/components/TeamsFormula1Results.js
+++ src/components/TeamsFormula1Results.js
@@ -1,58 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import * as $ from "jquery"
+
 
 const TeamsFormula1Results = () => {
-    const [formulaResults, getFormulaResults] = useState([]);
+    const [formulaDetails, setformulaDetails] = useState([]);
+    const [formulaResults, setFormulaResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const location = useLocation();
 
-
     useEffect(() => {
-        addFormulaResults();
+        order66();
     }, [])
 
-    const addFormulaResults = () => {
+    const order66 = async () => {
         const id = location.state.constructorId;
-        console.warn("this is id", id);
-        const url = `http://ergast.com/api/f1/2013/constructors/${id}/results.json`;
-
-        $.get(url, (data) => {
-            getFormulaResults(data.MRData.RaceTable.Races)
-            console.log("results", data)
-        })
+        const urlDetails = `http://ergast.com/api/f1/2013/constructors/${id}/constructorStandings.json`;
+        const urlResults = `http://ergast.com/api/f1/2013/constructors/${id}/results.json`;
+        const flags = `https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json`;
+        const responseDetails = await fetch(urlDetails);
+        const responseResults = await fetch(urlResults);
+        const detailsX = await responseDetails.json();
+        const resultsX = await responseResults.json();
+        setformulaDetails(detailsX.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+        setFormulaResults(resultsX.MRData.RaceTable.Races);
+        setIsLoading(false);
     }
-
+    if (isLoading) {
+        return (<h1>Is loading...</h1>);
+    }
     return (
-        <div>
-            <div></div>
 
-            {formulaResults.map(item => {
-                console.log("team formula results", item);
-                return (
-                <table>
-                    <thead>
-                        <tr>
-                            <td>Round</td>
-                            <td>Grand Prix</td>
-                            <td>{item.Results[0].Driver.familyName}</td>
-                            <td>{item.Results[1].Driver.familyName}</td>
-                            <td>Points</td>
-                        </tr>
-                    </thead>
+        <div>
+            <table>
+                {formulaDetails.map(item => {
+                    return(
                     <tbody>
                         <tr>
-                            <td>{item.round}</td>
-                            <td>{item.raceName}</td>
-                            <td>{item.Results[0].position}</td>
-                            <td>{item.Results[1].position}</td>
-                            <td>{parseInt(item.Results[0].points) + parseInt(item.Results[1].points)}</td>
+                            <td colSpan="2">{item.Constructor.name}</td>
+                            <td>Country:{item.Constructor.nationality}</td>
+                            <td>Position:{item.position}</td>
+                            <td>Points:{item.points}</td>
+                            <td><a href={item.Constructor.url} target="_blank">History</a></td>
                         </tr>
                     </tbody>
-                </table>
-                )
-            })}
-
-
+                    );
+                })}
+            </table>
+            <table>
+                <thead>
+                    <tr>Formula 1</tr>
+                    <tr>
+                        <th>Round</th>
+                        <th>Grand Prix</th>
+                        <th>{formulaResults[0].Results[0].Driver.familyName}</th>
+                        <th>{formulaResults[0].Results[1].Driver.familyName}</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+                {formulaResults.map((item=>{
+                    return(
+                        <tbody>
+                            <tr>
+                                <td>{item.round}</td>
+                                <td>{item.raceName}</td>
+                                <td>{item.Results[0].position}</td>
+                                <td>{item.Results[1].position}</td>
+                                <td>{parseInt(item.Results[0].points) + parseInt(item.Results[1].points)}</td>
+                            </tr>
+                        </tbody>
+                    );
+                }))}
+            </table>
         </div>
     );
 }
