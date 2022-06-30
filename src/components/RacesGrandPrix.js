693b1feda35379b70a8cf72c31f2ba6a18f6f98b
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-06-30 09:19:02 +0200

    driver, races all

--- src/components/RacesGrandPrix.js
+++ src/components/RacesGrandPrix.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { CircleLoader } from "react-spinners";
 
 const GrandPrix = () => {
+    const [grandPrix, setGrandPrix] = useState([]);
     const [races, setRaces] = useState([]);
     const [resultRaces, setResultRaces] = useState([]);
     const location = useLocation();
-   
+    const [isLoading, setIsLoading] = useState(true);
+
 
     useEffect(() => {
         getRaceDetails();
@@ -13,20 +16,45 @@ const GrandPrix = () => {
 
     const getRaceDetails = async () => {
         const id = location.state.circuitId;
+        const grandPrixUrl = `http://ergast.com/api/f1/2013/${id}/results.json`;
         const qualifyingUrl = `http://ergast.com/api/f1/2013/${id}/qualifying.json`;
-        const resultsUrl = `http://ergast.com/api/f1/2013/${id}/results.json`;
+        const resultsUrl = `http://ergast.com/api/f1/2013/${id}/results.json`;//dohvatiti prvu tabelu
+        const responseGrandPrix = await fetch(grandPrixUrl);
+        const prix = await responseGrandPrix.json();
         const responseQualifying = await fetch(qualifyingUrl);
         const qualifying = await responseQualifying.json();
         const responseResults = await fetch(resultsUrl);
         const results = await responseResults.json();
+        setGrandPrix(prix.MRData.RaceTable.Races);
         setRaces(qualifying.MRData.RaceTable.Races[0].QualifyingResults);
         setResultRaces(results.MRData.RaceTable.Races[0].Results);
+        setIsLoading(false)
     }
 
-    console.log("resultRaces", resultRaces)
+    // console.log("resultRaces", resultRaces)
+    if (isLoading) {
+        return (<CircleLoader size={70} color="green" />)
+    }
 
     return (
         <div>
+            <div>
+                {grandPrix.map(item => {
+                    return (
+                        <div key={item.Circuit.circuitId}>
+                            <h3>{item.raceName}</h3>
+                            <p>Country: {item.Circuit.Location.country}</p>
+                            <p>Location:{item.Circuit.Location.locality}</p>
+                            <p>Date:{item.date}</p>
+                            <p>Full report </p>
+
+                        </div>
+
+                    )
+                })}
+
+
+            </div>
             <h3>Qualifying Results</h3>
             <table>
                 <thead>
