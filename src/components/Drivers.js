fee9161260b8bf5c240d34f1e73ac4cfe0aeeb73
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-06-30 12:51:56 +0200

    drivers flag

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
-import * as $ from "jquery";
 import { useNavigate } from 'react-router-dom';
+import Flag from "react-flagkit";
 
 const Drivers = () => {
-    const [drivers, getDrivers] = useState([])
+    const [drivers, setDrivers] = useState([]);
+    const [flagsDetails, setFlags] = useState([])
     const navigate = useNavigate();
 
     useEffect(() => {
         addDrivers()
     }, [])
 
-    const addDrivers = () => {
+    const addDrivers = async () => {
         const url = "http://ergast.com/api/f1/2013/driverStandings.json"
-        $.get(url, (data) => {
-            getDrivers(data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
-        })
+        const urlFlags = `https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json`;
+        const responseUrl = await fetch(url);
+        const responseFlags = await fetch(urlFlags);
+        const urlX = await responseUrl.json();
+        const flagsX = await responseFlags.json();
+        setDrivers(urlX.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+        setFlags(flagsX)
     }
 
     const handleClickDetails = (driverId) => {
@@ -34,7 +39,15 @@ const Drivers = () => {
                         return (
                             <tr key={item.position}>
                                 <td>{item.position}</td>
-                                <td onClick={() => { handleClickDetails(item.Driver.driverId) }}>{item.Driver.givenName} {item.Driver.familyName}</td>
+                                <td onClick={() => { handleClickDetails(item.Driver.driverId) }}>
+                                {flagsDetails.map((flag,i) => {
+                                    if (item.Driver.nationality === flag.nationality) {
+                                        return <Flag key ={i} country={flag.alpha_2_code} />
+                                    }else if(item.Driver.nationality === "British" && flag.nationality === "British, UK") {
+                                        return (<Flag key ={i}country="GB" />)
+                                    }
+                                })}
+                                    {item.Driver.givenName} {item.Driver.familyName}</td>
                                 <td>{item.Constructors[0].name}</td>
                                 <td>{item.points}</td>
                             </tr>
--- src/components/DriversDetails.js
+++ src/components/DriversDetails.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { CircleLoader } from "react-spinners";
+import Flag from 'react-flagkit';
 
 const DriversDetails = () => {
     const [details, setDetails] = useState([]);
     const [races, setRaces] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [flagsDetails, setFlags] = useState([])
     const location = useLocation();
 
     useEffect(() => {
@@ -16,14 +18,18 @@ const DriversDetails = () => {
         const id = location.state.driverId;
         const driversUrl = `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`;
         const raceUrl = `http://ergast.com/api/f1/2013/drivers/${id}/results.json`;
-
+        const urlFlags = `https://raw.githubusercontent.com/Dinuks/country-nationality-list/master/countries.json`;
         const resposeDriver = await fetch(driversUrl);
         const driverDetails = await resposeDriver.json();
         const responseRace = await fetch(raceUrl);
         const raceDetails = await responseRace.json();
+        const responseFlags = await fetch(urlFlags)
+        const flagsX = await responseFlags.json();
         setDetails(driverDetails.MRData.StandingsTable.StandingsLists[0].DriverStandings)
         setRaces(raceDetails.MRData.RaceTable.Races)
+        setFlags(flagsX);
         setIsLoading(false)
+        console.warn("id", id)
     }
     
     if(isLoading) {
@@ -38,7 +44,13 @@ const DriversDetails = () => {
                     return (
                         <div key={driver.position}>
                             <img src={require(`./../img/drivers/${driver.Driver.driverId}.jpg`).default} />
-                            <img src={"./../img/drivers/Adrian_Sutil.jpg"} alt="" />
+                            <p>{flagsDetails.map((flag,i) => {
+                                    if (driver.Driver.nationality === flag.nationality) {
+                                        return <Flag key ={i} country={flag.alpha_2_code} />
+                                    }else if(driver.Driver.nationality === "British" && flag.nationality === "British, UK") {
+                                        return (<Flag key ={i}country="GB" />)
+                                    }
+                                })}</p>
                             <p>{driver.Driver.givenName}</p>
                             <p>{driver.Driver.familyName}</p>
                             <p>{driver.Driver.nationality}</p>
@@ -69,7 +81,16 @@ const DriversDetails = () => {
                         return (
                             <tr key={item.round}>
                                 <td>{item.round}</td>
-                                <td>{item.raceName}</td>
+                                <td>
+                                {/* {flagsDetails.map((flag,i) => {
+                                    if (races.Circuit.Location.country === flag.nationality) {
+                                        return <Flag key ={i} country={flag.alpha_2_code} />
+                                    }else if(races.Circuit.Location.country === "British" && flag.nationality === "British, UK") {
+                                        return (<Flag key ={i}country="GB" />)
+                                    }
+                                })} */}
+                                    {item.raceName}
+                                    </td>
                                 <td>{item.Results[0].Constructor.name}</td>
                                 <td>{item.Results[0].grid}</td>
                                 <td>{item.Results[0].position}</td>
