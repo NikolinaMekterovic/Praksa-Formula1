e41c731f511f6905bb9096ebef19a9a025a915ec
Nikolina Mekterovic <m.nikolina@hotmail.com>
2022-07-04 10:11:41 +0200

    img

--- src/App.js
+++ src/App.js
@@ -44,34 +44,27 @@ const App = () => {
                         </td>
                         <td className="td2">
                             <div>
-                                <table>
-                                    <tr>
-                                        <td>
-                                            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                                                <div className="container-fluid">
-                                                    <a className="navbar-brand" href="https://f1feederseries.com/">F-1 Feeder</a>
-                                                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                                                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                                                        <span className="navbar-toggler-icon"></span>
-                                                    </button>
-                                                    <div className="collapse navbar-collapse" id="navbarCollapse">
-                                                        <ul className="navbar-nav me-auto mb-2 mb-md-0">
-                                                            <li className="nav-item">
-                                                                <a className="nav-link active" aria-current="page" href="./drivers">Drivers</a>
-                                                            </li>
-                                                        </ul>
-                                                        <form className="d-flex">
-                                                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                                            </input>
-                                                            <button className="btn btn-outline-success" type="submit">Search</button>
-                                                        </form>
-                                                    </div>
-                                                </div>
-                                            </nav>
-                                        </td>
-                                    </tr>
-                                </table>
-
+                                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                                    <div className="container-fluid">
+                                        <a className="navbar-brand" href="https://f1feederseries.com/">F-1 Feeder</a>
+                                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                                            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                                            <span className="navbar-toggler-icon"></span>
+                                        </button>
+                                        <div className="collapse navbar-collapse" id="navbarCollapse">
+                                            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                                                <li className="nav-item">
+                                                    <a className="nav-link active" aria-current="page" href="/">Drivers</a>
+                                                </li>
+                                            </ul>
+                                            <form className="d-flex">
+                                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                                </input>
+                                                <button className="btn btn-outline-success" type="submit">Search</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </nav>
                                 <Routes>
                                     <Route path="/" element={<Drivers />}></Route>
                                     <Route path="/races" element={<Races />}></Route>
